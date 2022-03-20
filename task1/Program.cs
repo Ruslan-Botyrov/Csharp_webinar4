803ce7da8ee827f9d7efe03a79834b5f847914e8
Ruslan <botyrovra@gmail.com>
2022-03-20 12:00:14 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// принять число А и выдать сумму чисел от 1 до А
+
+
+int number = Prompt("Введите число");
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {NumberSumm(number)}");
+
+int NumberSumm(int digit)
+{
+    int sum = 0;
+    for (int i = 1; i <= digit; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
