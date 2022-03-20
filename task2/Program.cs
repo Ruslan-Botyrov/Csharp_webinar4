844094f7bef02047c1503af0ebe14f76d800fc59
Ruslan <botyrovra@gmail.com>
2022-03-20 12:00:31 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 
+
+//Console.WriteLine("введите число");
+//string strValue = Console.ReadLine();
+
+//int strLen = strValue.Length;
+//Console.WriteLine(strLen);
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int number = Prompt("Введите число ");
+//пока число больше 1 делим его на 10 и к счетчику прибавляем 1
+int counter = 0;
+while (number >= 1)
+{
+    number = number / 10;
+    counter++;
+
+}
+Console.WriteLine(counter);
