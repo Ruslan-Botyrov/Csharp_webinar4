348cd8fa8ba2fcac4c7ab66f2dfeec2fe2706b35
Ruslan <botyrovra@gmail.com>
2022-03-26 23:12:38 +0300

    add homework2v2

---
+++ homework2v2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через перевод в массив и сумму элементов массива.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+
+int number = Prompt("Введите число ");
+char[] arr = number.ToString().ToCharArray();
+
+int sum = 0;
+for (int i = 0; i < (arr.Length-1); i++)
+{
+    sum += arr[i];
+}
+
+Console.WriteLine(sum); //так не получается, не понимаю почему :(
+    
