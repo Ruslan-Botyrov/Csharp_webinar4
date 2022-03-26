d795ac3d7f8f152c760a3c80dd4175a4a457c587
Ruslan <botyrovra@gmail.com>
2022-03-26 23:12:17 +0300

    add homework1v2

---
+++ homework1v2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int numberA = Prompt("Введите число А ");
+int numberB = Prompt("Введите число B ");
+
+int pow = 1;
+for (int i =0; i<numberB; i++){
+    pow *=numberA;
+}
+Console.WriteLine($"{numberA}^{numberB} = {pow}");
